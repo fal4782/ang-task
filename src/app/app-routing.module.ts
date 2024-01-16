d8f32ba25b7bba18940d41a519f975d12e859046
fal4782 <falguni4782@gmail.com>
2024-01-16 11:52:51 +0530

    menu working properly on search

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { IssueEticketComponent } from './content/issue-eticket/issue-eticket.component';
 import { AddSuspensionReversalComponent } from './content/add-suspension-reversal/add-suspension-reversal.component';
+import { HomeComponent } from './content/home/home.component';
 
 @NgModule({
   imports: [
     RouterModule.forRoot(
       [
+        { path: '', component: HomeComponent},
         { path: 'ticket/issue', component: IssueEticketComponent },
         { path: 'suspension/reissue-notice-to-taj/reveral-of-suspension-add',component: AddSuspensionReversalComponent}
       ],
