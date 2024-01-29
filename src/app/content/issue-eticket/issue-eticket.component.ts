acbf6c9bfe86da7f5edeca3e1e2921225e301776
fal4782 <falguni4782@gmail.com>
2024-01-29 15:24:10 +0530

    issue eTicket table added

--- src/app/content/issue-eticket/issue-eticket.component.ts
+++ src/app/content/issue-eticket/issue-eticket.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { CommonService } from 'src/app/shared/services/common.service';
+
+interface Warrant {
+  warrantRefNo: number;
+  issueDate: Date;
+  idNumber: number; //dl number
+  firstName: string;
+  middleName: string;
+  lastname: string;
+  courtName: string;
+  courtHearingDate: Date;
+  warrantDate: Date;
+  //   printStatus: string;
+  //   warrantState: string;
+  customField?: string;
+}
 
 @Component({
   selector: 'app-issue-eticket',
   templateUrl: './issue-eticket.component.html',
-  styleUrls: ['./issue-eticket.component.scss']
+  styleUrls: ['./issue-eticket.component.scss'],
 })
 export class IssueEticketComponent {
+  warrants: Warrant[];
+  selectedWarrants: Warrant[] = [];
 
+  constructor(private commonService: CommonService) {}
+
+  prerequisite() {
+    this.getWarrantsList();
+  }
+
+  ngOnInit() {
+    this.prerequisite();
+  }
+
+  getWarrantsList() {
+    this.commonService.getWarrantsData().subscribe((data) => {
+      this.warrants = data;
+      this.warrants = data.map((warrant) => ({ ...warrant, customField: '' }));
+    });
+  }
+
+  isSelected(warrant: Warrant): boolean {
+    return this.selectedWarrants.includes(warrant);
+  }
 }
